73c4289cc44da696879d6e299215c23878477912
Hambak-note <dudghks5722@gmail.com>
2022-12-15 21:33:42 +0900

    project setting and add practice code:

---
+++ Java/the-java/src/main/java/codemaniplulation/thejava/Moim.java
@@ -0,0 +1,25 @@
+package codemaniplulation.thejava;
+
+public class Moim {
+
+    public static void main(String[] args) {
+
+    }
+
+    int maxNumberOfAttendees;
+
+    int numberOfEnrollment;
+
+    public boolean isEnrollmentFull(){
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+
+        if(numberOfEnrollment < maxNumberOfAttendees){
+            return false;
+        }
+
+        return true;
+    }
+
+}
