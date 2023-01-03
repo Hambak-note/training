abe73f75a27a0e3da7bc475bc16b127267d10f4a
Hambak-note <dudghks5722@gmail.com>
2023-01-03 15:54:13 +0900

    feat: Add pratice code about cookie, session

---
+++ Spring/login-start/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,38 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    //@ModelAttribute("member")를 @ModelAttribute 로 변경해도 결과는 같다.
+    //강의에서는 IDE 인식 문제가 있어서 적용.
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/addMembrerForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
