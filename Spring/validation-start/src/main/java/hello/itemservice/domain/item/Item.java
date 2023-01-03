4485e60d1827cdcfb204f13daccef73d444f283d
Hambak-note <dudghks5722@gmail.com>
2023-01-03 13:05:21 +0900

    feat: Add practice code about been validation

--- Spring/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,6 +1,13 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
