c756e7fa3d124a13708650dc577ff55edf0a33eb
Hambak-note <dudghks5722@gmail.com>
2023-01-12 11:26:49 +0900

    Add V3 버전의 필드 동기화 문제를 해결하는 FieldLogTrace 추가 및 적용

---
+++ Advanced-spring-core-principles/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,19 @@
+package hello.advanced;
+
+import hello.advanced.trace.logtrace.FieldLogTrace;
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 필드 동기화 적용을 위해 LogTrace 스프링 빈 등록
+ */
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace(){
+        return new FieldLogTrace();
+    }
+
+}
